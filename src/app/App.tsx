93c5688669e5d492d2f4fc172b3812cea4fee61b
Artyom Varibrus <varibrus.artyom@gmail.com>
2024-02-28 00:44:17 +0300

    Creating a color theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from "@mui/material/styles"
+import { theme } from "./styles/theme"
+import { BasePage } from "../pages/base_page/BasePage"
+
 function App() {
 
   return (
-    <>
-    </>
+    <ThemeProvider theme={theme}>
+      <BasePage />
+    </ThemeProvider >
   )
 }
 

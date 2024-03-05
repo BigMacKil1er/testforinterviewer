d69ee476bbf74ae94d980506a2f8b186eb136126
Artyom Varibrus <varibrus.artyom@gmail.com>
2024-03-05 18:24:09 +0300

    Typing and repeated requests

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -1,24 +1,67 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react'
 import { TOKEN } from './lib/auth'
+
 const BASE_URL = 'http://api.valantis.store:40000/'
 
-const baseQuery = fetchBaseQuery({
+interface IResponseId {
+    result: string[]
+}
+
+type field = 'price' | 'id' | 'brand' | 'product'
+
+type actions = 'get_ids' | 'get_items' | 'get_fields' | 'filter'
+
+interface IParamsField {
+    field: field, 
+    offset: number, 
+    limit: number
+}
+interface IParamsIds extends Omit<IParamsField, field> {}
+
+type paramsItems = {
+    ids: string[]
+}
+
+type paramsFilter = {
+    [Key in field]: number | string
+}
+
+type ParamsByAction<T extends actions> = T extends 'filter' ? paramsFilter : T extends 'get_fields' ? IParamsField : T extends 'get_ids' ? IParamsIds : paramsItems
+
+interface Credentials<T extends actions> {
+    action: T,
+    params: ParamsByAction<T>
+}
+
+type itemData = {
+    brand: string | null,
+    id: string,
+    price: number,
+    product: string
+}
+
+interface IResponseItems {
+    result: itemData[]
+}
+
+const baseQuery = retry(fetchBaseQuery({
     baseUrl: `${BASE_URL}`,
     prepareHeaders: (headers) => {
         headers.set("X-Auth", TOKEN)
         return headers
     }
-})
+}), {maxRetries: 5})
+
 
 export const apiSlice = createApi({
     baseQuery: baseQuery,
     endpoints: builder => ({
-        respondent: builder.mutation({
+        respondent: builder.mutation<IResponseId | IResponseItems, Credentials<actions>>({
             query: (credentials) => ({
                 url: '',
                 method: 'POST',
                 body: credentials,
-            })
+            }),
         })
-    })
+    }),
 })

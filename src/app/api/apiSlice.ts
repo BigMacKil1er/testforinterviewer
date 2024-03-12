1aa15049b3c4d87d4c1f495f59ebbc18040d99de
Artyom Varibrus <varibrus.artyom@gmail.com>
2024-03-12 14:51:41 +0300

    query bug correction

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -106,15 +106,9 @@ export const apiSlice = createApi({
             async queryFn(arg, queryApi, _extraOptions, fetchWithBQ){
                 const state = queryApi.getState() as RootState
                 const dispatch = queryApi.dispatch
-                
                 const offset = arg.params.offset ? arg.params.offset : 0
                 const limit = arg.params.limit ? arg.params.limit : 50
                 let fullData:itemObj[] = []
-                
-                if (state.queryStatus.isGetItemsInProgress) {
-                    fullData = []
-                    return { data: { result: fullData } }
-                }
                 const result = await fetchWithBQ({
                     url: '',
                     method: 'POST',
@@ -126,34 +120,32 @@ export const apiSlice = createApi({
                 
                 const data = result.data as {result: string[]}
                 for (let loopOffset = 0; loopOffset < data.result.length; loopOffset += 100) {
-                    const stateinner = queryApi.getState() as RootState
-                    if (stateinner.queryStatus.isGetItemsInProgress) {
-                        fullData = []
-                        break
-                    }
-                        const itemResult = await fetchWithBQ({
-                            url: '',
-                            method: 'POST',
-                            body: 
-                                {
-                                    "action": "get_items",
-                                    "params": {"ids": data.result.slice(loopOffset, loopOffset + 100)}
-                                }
-                        })
-                        if (itemResult.data instanceof Object && 'result' in itemResult.data) {
-                            const data = itemResult.data as { result: itemObj[] };
-                            const filteredResult = deleteDouble(data.result)
-                            dispatch(setItems({result: [...state.items.result, ...fullData]}))
-                            fullData.push(...filteredResult)
+                    
+                    const itemResult = await fetchWithBQ({
+                        url: '',
+                        method: 'POST',
+                        body: 
+                            {
+                                "action": "get_items",
+                                "params": {"ids": data.result.slice(loopOffset, loopOffset + 100)}
+                            }
+                    })
+                    if (itemResult.data instanceof Object && 'result' in itemResult.data) {
+                        const stateinner = queryApi.getState() as RootState
+                        if (stateinner.queryStatus.isGetItemsInProgress) {
+                            fullData = []
+                            break
                         }
+                        const data = itemResult.data as { result: itemObj[] };
+                        const filteredResult = deleteDouble(data.result)
+                        dispatch(setItems({result: [...state.items.result, ...fullData]}))
+                        fullData.push(...filteredResult)
+                    }
                     
                 }
                 if (fullData.length > 0) {
                     dispatch(setItems({result: fullData}))
                 }
-                setTimeout(()=>{
-                    dispatch(setQueryStatus(false))
-                },1000)
                 return { data: { result: fullData } }
             }
         }
